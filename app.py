2d070157667cf528e52df3432f1924bb9deacfe0
Dan Gyi <daniel.gyi@gmail.com>
2019-11-15 13:12:06 +0000

    update app to import deprecated tensorflow

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import os
 
 x1 = tf.constant([1,2,3,4])
 x2 = tf.constant([5,6,7,8])
@@ -22,8 +24,12 @@ def load_data(data_directory):
       labels.append(int(d))
   return images, labels
 
-  ROOT_PATH = '/Users/dangyi/Documents/Projects/tensorflow-intro'
-  train_data_directory = os.path.join(ROOT_PATH, "Training")
-  test_data_directory = os.path.join(ROOT_PATH, "Testing")
+ROOT_PATH = '/Users/dangyi/Documents/Projects/tensorflow-intro'
+train_data_directory = os.path.join(ROOT_PATH, "Training")
+test_data_directory = os.path.join(ROOT_PATH, "Testing")
 
-  images, labels = load_data(train_data_directory)
+images, labels = load_data(train_data_directory)
+
+print(images.ndim)
+print(images.size)
+images[0]
