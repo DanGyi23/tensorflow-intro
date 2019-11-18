32ebc8dacfd9ea084d8605fe39c866820974e136
Dan Gyi <daniel.gyi@gmail.com>
2019-11-18 10:14:14 +0000

    shuffling and batching datasets

--- app.py
+++ app.py
@@ -19,3 +19,19 @@ def format_example(image, label):
 train = raw_train.map(format_example)
 validation = raw_validation.map(format_example)
 test = raw_test.map(format_example)
+
+def augment_data(image, label):
+        image = tf.image.random_flip_left_right(image)
+        image = tf.image.random_contrast(image, lower=0.0, upper=1.0)
+        return image, label
+
+train = train.map(augment_data)
+
+def shuffle_and_batch(dataset):
+        ds = dataset.apply( tf.data.experimental.shuffle_and_repeat(buffer_size=218))
+        ds = ds.batch(32)
+        ds = ds.prefetch(buffer_size=218)
+
+shuffle_and_batch(train)
+shuffle_and_batch(validation)
+shuffle_and_batch(test)
