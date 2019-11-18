307764adcee4dfa90d3b4853b5085e839238d7a8
Dan Gyi <daniel.gyi@gmail.com>
2019-11-18 09:19:30 +0000

    no compile errors

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import tensorflow_datasets as tfds
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 SPLIT_WEIGHTS = (8, 1, 1)
 splits = tfds.Split.TRAIN.subsplit(weighted=SPLIT_WEIGHTS)
@@ -10,7 +13,7 @@ splits = tfds.Split.TRAIN.subsplit(weighted=SPLIT_WEIGHTS)
 def format_example(image, label):
         image = tf.cast(image, tf.float32)
         image = image / 255.0
-        image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+        image = tf.image.resize(image, (255, 255))
         return image, label
         
 train = raw_train.map(format_example)
