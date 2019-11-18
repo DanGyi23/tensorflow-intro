b9bc0f55e920d2fd5e673f442f0051152ada0e2b
Dan Gyi <daniel.gyi@gmail.com>
2019-11-18 10:27:53 +0000

    Keras model added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
+from tensorflow import keras
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -35,3 +36,18 @@ def shuffle_and_batch(dataset):
 shuffle_and_batch(train)
 shuffle_and_batch(validation)
 shuffle_and_batch(test)
+
+def create_model():
+        img_inputs = keras.Input(shape=IMG_SHAPE)
+        conv_1 = keras.layers.Conv2D(32, (3,3), activation=relu)(img_inputs)
+        maxpool_1 = keras.layers.Maxpooling2D((2,2))(conv_1)
+        conv_2 = keras.layers.Conv2D(64, (3, 3), activation='relu')(maxpool_1)
+        maxpool_2 = keras.layers.MaxPooling2D((2, 2))(conv_2)
+        conv_3 = keras.layers.Conv2D(64, (3, 3), activation='relu')(maxpool_2)
+        flatten = keras.layers.Flatten()(conv_3)
+        dense_1 = keras.layers.Dense(64, activation='relu')(flatten)
+        output = keras.layers.Dense(metadata.features['label'].num_classes, activation='softmax')(dense_1)
+
+        model = keras.Model(inputs=img_inputs, outputs=output)
+
+        return model
