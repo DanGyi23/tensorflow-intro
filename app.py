386bcff298396d4ee42f43c27aeede437e14d232
Dan Gyi <daniel.gyi@gmail.com>
2019-11-18 09:09:05 +0000

    update app for image resizing

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ splits = tfds.Split.TRAIN.subsplit(weighted=SPLIT_WEIGHTS)
                                                             with_info=True,
                                                             split=list(splits),                                       
                                                             as_supervised=True)
+
+def format_example(image, label):
+        image = tf.cast(image, tf.float32)
+        image = image / 255.0
+        image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+        return image, label
+        
+train = raw_train.map(format_example)
+validation = raw_validation.map(format_example)
+test = raw_test.map(format_example)
