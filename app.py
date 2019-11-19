30718485861ed6451c9198c50ebfa598bf444396
Dan Gyi <daniel.gyi@gmail.com>
2019-11-19 10:09:41 +0000

    update imports

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import cv2
 import numpy as np
 from random import shuffle
 from tqdm import tqdm
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
+# from tflearn.layers.conv import conv_2d, max_pool_2d
+# from tflearn.layers.core import input_data, dropout, fully_connected
+# from tflearn.layers.estimator import regression
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
