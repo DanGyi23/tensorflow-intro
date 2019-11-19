aca96082e1ba78ae79764e9a03b91324740667fc
Dan Gyi <daniel.gyi@gmail.com>
2019-11-19 09:13:02 +0000

    update imports

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import tensorflow_datasets as tfds
 import tensorflow as tf
 from tensorflow import keras
 import os
+import cv2                
+import numpy as np
+from random import shuffle
+from tqdm import tqdm
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 BATCH_SIZE = 32
