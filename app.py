d7414aae4f04efce893fc71d3c8d049a46883a67
Dan Gyi <daniel.gyi@gmail.com>
2019-11-20 11:28:09 +0000

    generates image transformations in preview

--- app.py
+++ app.py
@@ -1,51 +1,22 @@
-import tensorflow_datasets as tfds
-import tensorflow as tf
-from tensorflow import keras
-import os
-import cv2                
-import numpy as np
-from random import shuffle
-from tqdm import tqdm
-from tensorflow.python.compiler.tensorrt import trt_convert as trt
-# from tflearn.layers.conv import conv_2d, max_pool_2d
-# from tflearn.layers.core import input_data, dropout, fully_connected
-# from tflearn.layers.estimator import regression
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
 
-TRAIN_DIR = '/Users/dangyi/Downloads/dogs-vs-cats/train'
-TEST_DIR = '/Users/dangyi/Downloads/dogs-vs-cats/test'
-IMG_SIZE = 50
-LR = 1e-3
+img = load_img('dogs-vs-cats/train/cat.0.jpg')  # this is a PIL image
+x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
 
-MODEL_NAME = 'dogsvscats-{}-{}.model'.format(LR, '2conv-basic')
-
-def label_img(img):
-        word_label = img.split('.')[-3]
-        if word_label == 'cat': return [1,0]
-        elif word_label == 'dog': return [0,1]
-
-def create_train_data():
-        training_data = []
-        for img in tqdm(os.listdir(TRAIN_DIR)):
-                label = label_img(img)
-                path = os.path.join(TRAIN_DIR,img)
-                img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
-                img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
-                training_data.append([np.array(img),np.array(label)])
-        shuffle(training_data)
-        np.save('train_data.npy', training_data)
-        return training_data
-
-def process_test_data():
-        testing_data = []
-        for img in tqdm(os.listdir(TEST_DIR)):
-                path = os.path.join(TEST_DIR,img)
-                img_num = img.split('.')[0]
-                img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
-                img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
-                testing_data.append([np.array(img), img_num])
-                
-        shuffle(testing_data)
-        np.save('test_data.npy', testing_data)
-        return testing_data
+# the .flow() command below generates batches of randomly transformed images
+# and saves the results to the `preview/` directory
+i = 0
+for batch in datagen.flow(x, batch_size=1, save_to_dir='preview', save_prefix='cat', save_format='jpeg'):
+        i += 1
+        if i > 20:
+                break  # otherwise the generator would loop indefinitely
